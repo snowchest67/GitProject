007e48d057f5def6072f672ed526f6980c0fd156
snowchest67 <it.is.for.snowchest67@tut.by>
2024-11-15 21:17:43 +0300

    Добавил поля и конструкторы

--- ResearchTeam.cs
+++ ResearchTeam.cs
@@ -8,6 +8,27 @@ namespace GitProject
 {
     internal class ResearchTeam
     {
+        private string researchTopics;
+        private string nameOrganizationr;
+        private int id;
+        private DateTime duration;
+        //private Paper[] papers;
 
+        public ResearchTeam(string researchTopics, string nameOrganizationr, int id, DateTime duration)
+        {
+            this.researchTopics = researchTopics;
+            this.nameOrganizationr = nameOrganizationr;
+            this.id = id;
+            this.duration = duration;
+            //Paper[] papers = papers;
+        }
+
+        public ResearchTeam()
+        {
+            this.researchTopics = "researchTopics";
+            this.nameOrganizationr = "nameOrganizationr";
+            this.id = 0;
+            //this.duration = 0;
+        }
     }
 }
