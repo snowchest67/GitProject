8c9713082ba58c62c1a3de0a96c620f6194bfda6
ZlataGrosul <zlatagr2006@gmail.com>
2024-11-17 17:59:09 +0300

    Добавила открытые свойства, доступные для чтения и записи (название публикации, автор, дата публикации).

--- Paper.cs
+++ Paper.cs
@@ -8,5 +8,8 @@ namespace GitProject
 {
     internal class Paper
     {
+        public string titleOfPaper { get; set; }//открытое свойство типа стринг для названия публикации
+        public Person author { get; set; }//открытое свойство типа Person для автора
+        public DateTime dateOfPaper { get; set; }//открытое свойство типа DateTime для даты публикации
     }
 }
