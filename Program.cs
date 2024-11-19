56d77c9edee97aa61d088fe4e3e3bb983e1d1616
ZlataGrosul <zlatagr2006@gmail.com>
2024-11-19 12:35:40 +0300

    Вывести значения индексатора для значений индекса TimeFrame.Year, TimeFrame.TwoYears, TimeFrame.Long.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ try
 {
     ResearchTeam r1 = new ResearchTeam("Автоматы по экзаменам.", "КБиП", 291, TimeFrame.Long);
     Console.WriteLine(r1.ToShortString());
+    Console.WriteLine("Значение индексатора для TimeFrame.Long: " + r1[TimeFrame.Long]);
+    Console.WriteLine("Значение индексатора для TimeFrame.Long: " + r1[TimeFrame.Year]);
+    Console.WriteLine("Значение индексатора для TimeFrame.Long: " + r1[TimeFrame.TwoYears]);
 }
 catch (ArgumentNullException ex)
 {
