97a6819f3c2fe3e4b432a69bf27a201afbd1c7fa
ZlataGrosul <zlatagr2006@gmail.com>
2024-12-07 12:31:24 +0300

    Добавила интерфейс, в нем определила свойство Name, и object

---
+++ INameAndCopy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    internal interface INameAndCopy
+    {
+        string Name { get; set; }
+        object DeepCopy();
+    }
+}
